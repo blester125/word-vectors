d01e442273aea8925c8ccd5cd7cf4bae23edac05
Brian Lester <blester125@gmail.com>
2018-07-05 20:46:08 -0400

    added benchmark

--- tests/utils.py
+++ tests/utils.py
@@ -14,18 +14,5 @@ scale = np.reshape(np.arange(VOCAB, dtype=np.float32), (-1, 1))
 vectors = vectors + scale
 
 
-def main():
-    with open(DATA / GLOVE, 'w') as f:
-        for word, vector in zip(vocab, vectors):
-            f.write(" ".join([word, *map(str, vector)]) + "\n")
-
-    with open(DATA / W2V, 'wb') as f:
-        f.write(f"{len(vocab)} {dim}\n".encode('utf-8'))
-        for word, vector in zip(vocab, vectors):
-            f.write((word + " ").encode('utf-8') + vector.tobytes())
-
-    write_dense(vocab, vectors, 2, DATA / DENSE)
-
-
 if __name__ == '__main__':
     main()
