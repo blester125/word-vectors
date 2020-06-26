218943c4c813c3980e571c8a7376c7ae48c3f2a8
Brian Lester <blester125@gmail.com>
2020-06-26 11:21:23 -0400

    chore: processed vocabulary stats and distributions

--- benchmark/token-distributions.py
+++ benchmark/token-distributions.py
@@ -1,3 +1,5 @@
+import os
+import json
 import argparse
 from itertools import chain
 from collections import Counter
@@ -29,32 +31,71 @@ def std(x: CounterType[int], ddof: int = 1) -> float:
     return np.std(list(chain(*([k] * v for k, v in x.items()))), ddof=ddof)
 
 
-def plot_distribution(x: CounterType[int]):
+def plot_distribution(x: CounterType[int], title: str = "Type length distribution", output: str = "distribution.png"):
     norm = sum(x.values())
     x = Counter({k: x[k] / norm for k in range(max(x))})
-    plt.bar(range(max(x)), [x[k] for k in range(max(x))])
+    plt.bar(range(max(x)), [x[k] for k in range(max(x))], width=1)
+    plt.title(title)
+    plt.xlabel("Type Length")
+    plt.ylabel("Proportion of vocabulary")
+    plt.savefig(output)
     plt.show()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("embedding")
+    parser.add_argument("--embed-name", "--embed_name", help="Force a name if the file name isn't descriptive")
     parser.add_argument("--format", type=FileType.from_string)
+    parser.add_argument(
+        "--output",
+        help="The name of the output files sans extensions. Will create a .json or stats and a .png the the distirbution graph",
+    )
     args = parser.parse_args()
 
     v, wv = read(args.embedding, args.format)
 
     lengths = Counter(len(token.encode("utf-8")) for token in v)
 
-    print(f"Vocab size: {len(v)}")
-    print(f"Vector size: {wv.shape[-1]}")
-    print(f"Shortest token: {min(lengths)}")
-    print(f"Longest token: {max(lengths)}")
-    print(f"Average token length: {mean(lengths)}")
-    print(f"Std of token length: {std(lengths)}")
-    print(f"Percentage of padding bytes: {wasted_bytes(lengths) / total_bytes(lengths) * 100:.4f}%")
+    vsz = len(v)
+    dsz = wv.shape[-1]
+    min_l = min(lengths)
+    max_l = max(lengths)
+    avg_l = mean(lengths)
+    std_l = std(lengths)
+    p_pad = wasted_bytes(lengths) / total_bytes(lengths) * 100
 
-    plot_distribution(lengths)
+    print(f"Vocab size: {vsz}")
+    print(f"Vector size: {dsz}")
+    print(f"Shortest token: {min_l}")
+    print(f"Longest token: {max_l}")
+    print(f"Average token length: {avg_l}")
+    print(f"Std of token length: {std_l}")
+    print(f"Percentage of padding bytes: {p_pad:.4f}%")
+
+    embed_name = os.path.splitext(os.path.basename(args.embedding))[0] if args.embed_name is None else args.embed_name
+
+    data = {
+        "name": embed_name,
+        "stats": {
+            "vocab_size": vsz,
+            "vector_size": dsz,
+            "min_length": min_l,
+            "max_length": max_l,
+            "avg_length": avg_l,
+            "std_length": std_l,
+            "percent_pad": p_pad,
+        },
+        "counts": {**lengths},
+    }
+
+    if args.output is None:
+        args.output = embed_name
+
+    with open(f"{args.output}.json", "w") as wf:
+        json.dump(data, wf, indent=2)
+
+    plot_distribution(lengths, title=f"Type Length Distribution for {embed_name}", output=f"{args.output}.png")
 
 
 if __name__ == "__main__":
